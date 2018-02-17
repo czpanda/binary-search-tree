b755cd6d2610f32ee71b1a1c29c454bdc22dfcf2
Frantisek Musil <frantisek.musil@educanet.cz>
2018-02-17 17:20:55 +0100

    Add beforeEach method

--- test/NodeTest.java
+++ test/NodeTest.java
@@ -1,26 +1,31 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NodeTest {
+    private Node tree;
+
+    @BeforeEach
+    public void beforeEach() {
+        this.tree = new Node(8);
+
+        tree.add(5);
+        tree.add(17);
+        tree.add(2);
+        tree.add(7);
+        tree.add(12);
+        tree.add(20);
+        tree.add(4);
+    }
+
     @Test
     public void testAdd() {
-        Node node = new Node(8);
-
-        node.add(5);
-        node.add(17);
-        node.add(2);
-        node.add(7);
-        node.add(12);
-        node.add(20);
-        node.add(4);
-
-        assertEquals(8, node.value);
-        assertEquals(5, node.left.value);
-        assertEquals(17, node.right.value);
-        assertEquals(2, node.left.left.value);
-        assertEquals(7, node.left.right.value);
-        assertEquals(12, node.right.left.value);
-        assertEquals(20, node.right.right.value);
-        assertEquals(4, node.left.left.right.value);
+        assertEquals(8, tree.value);
+        assertEquals(5, tree.left.value);
+        assertEquals(17, tree.right.value);
+        assertEquals(2, tree.left.left.value);
+        assertEquals(7, tree.left.right.value);
+        assertEquals(12, tree.right.left.value);
+        assertEquals(20, tree.right.right.value);
+        assertEquals(4, tree.left.left.right.value);
     }
 }
