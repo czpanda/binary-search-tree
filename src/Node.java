6dd88aae1a6631a8ebc861a568ac0d85c71f8fb8
Frantisek Musil <frantisek.musil@educanet.cz>
2018-02-19 21:03:39 +0100

    Add remove method & fix toString method & refactor code
    Changes introduced byt this commit :
    1. Add remove method
    2. Change Node value property to public and not final as it needs
       to be accessed by remove method
    3. Fix toString method
    4. Increase number of tested Nodes
    5. Overall code cleanup

--- src/Node.java
+++ src/Node.java
@@ -1,5 +1,6 @@
 public class Node {
-    public final int value;
+
+    public int value;
 
     public Node left;
 
@@ -10,9 +11,6 @@ public class Node {
     }
 
     public void add(int value) {
-        /*
-         * Skip value if it already exists in a tree
-         */
         if (this.value == value) {
             System.out.println("Value already exists in a tree, therefore it will be skipped.");
             return;
@@ -74,16 +72,64 @@ public class Node {
         return this.left.min();
     }
 
+    public void remove(int value) {
+        // Value to be removed is the left link of current node
+        if (this.left != null && this.left.value == value) {
+            // Node has no children
+            if (this.left.left == null && this.left.right == null) {
+                this.left = null;
+                return;
+            }
+
+            // Node has one child
+            if (this.left.left != null && this.left.right == null || this.left.left == null) {
+                this.left = this.left.left;
+                return;
+            }
+
+            // Node has two children
+            int min = this.left.right.min();
+            this.left.value = min;
+            this.left.remove(min);
+        }
+
+        // Value to be removed is the right link of current node
+        if (this.right != null && this.right.value == value) {
+            // Node has no children
+            if (this.right.left == null && this.right.right == null) {
+                this.right = null;
+                return;
+            }
+
+            // Node has one child
+            if (this.right.left != null && this.right.right == null || this.right.left == null) {
+                this.right = this.right.right;
+                return;
+            }
+
+            // Node has two children
+            int min = this.right.min();
+            this.right.value = min;
+            this.right.right.remove(min);
+        }
+
+        if (this.value < value && this.right != null) {
+            this.right.remove(value);
+        } else if (this.value > value && this.left != null){
+            this.left.remove(value);
+        }
+    }
+
     @Override
     public String toString(){
-        if (this.left == null && this.right == null) {
-            return Integer.toString(this.value);
-        } else if (this.left == null) {
-            return Integer.toString(this.value) + "-" + this.right.toString();
-        } else if (this.right == null) {
-            return Integer.toString(this.value) + "-" + this.left.toString();
-        } else {
+        if (this.left != null && this.right != null) {
             return this.left.toString() + "-" + Integer.toString(this.value) + "-" + this.right.toString();
+        } else if (this.left != null) {
+            return this.left.toString() + "-" + Integer.toString(this.value);
+        } else if (this.right != null) {
+            return Integer.toString(this.value) + "-" + this.right.toString();
+        } else {
+            return Integer.toString(this.value);
         }
     }
 
--- test/NodeTest.java
+++ test/NodeTest.java
@@ -1,17 +1,20 @@
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class NodeTest {
+class NodeTest {
+
     private Node firstTree;
 
     private Node secondTree;
 
     private Node thirdTree;
 
+    private Node fourthTree;
+
 
     @BeforeEach
     public void beforeEach() {
-        this.firstTree = new Node(10);
+        firstTree = new Node(10);
 
         firstTree.add(15);
         firstTree.add(0);
@@ -20,7 +23,7 @@ public class NodeTest {
         firstTree.add(5);
         firstTree.add(8);
 
-        this.secondTree = new Node(8);
+        secondTree = new Node(8);
 
         secondTree.add(5);
         secondTree.add(17);
@@ -29,8 +32,25 @@ public class NodeTest {
         secondTree.add(12);
         secondTree.add(20);
         secondTree.add(4);
+        secondTree.add(1);
+        secondTree.add(25);
+        secondTree.add(15);
 
-        this.thirdTree = new Node(50);
+        thirdTree = new Node(50);
+
+        thirdTree.add(20);
+        thirdTree.add(15);
+        thirdTree.add(22);
+        thirdTree.add(10);
+        thirdTree.add(25);
+        thirdTree.add(90);
+        thirdTree.add(70);
+        thirdTree.add(95);
+        thirdTree.add(100);
+        thirdTree.add(92);
+
+        fourthTree = new Node(50);
+
     }
 
     @AfterEach
@@ -40,11 +60,12 @@ public class NodeTest {
         this.secondTree = null;
 
         this.thirdTree = null;
+
+        this.fourthTree = null;
     }
 
     @Test
     public void testAdd() {
-        // First tree asserts
         assertEquals(10, firstTree.value);
         assertEquals(15, firstTree.right.value);
         assertEquals(0, firstTree.left.value);
@@ -52,7 +73,6 @@ public class NodeTest {
         assertEquals(5, firstTree.left.right.value);
         assertEquals(8, firstTree.left.right.right.value);
 
-        // Second tree asserts
         assertEquals(8, secondTree.value);
         assertEquals(5, secondTree.left.value);
         assertEquals(17, secondTree.right.value);
@@ -61,75 +81,124 @@ public class NodeTest {
         assertEquals(12, secondTree.right.left.value);
         assertEquals(20, secondTree.right.right.value);
         assertEquals(4, secondTree.left.left.right.value);
+        assertEquals(1, secondTree.left.left.left.value);
+        assertEquals(15, secondTree.right.left.right.value);
+        assertEquals(25, secondTree.right.right.right.value);
 
-        // Third tree asserts
         assertEquals(50, thirdTree.value);
+        assertEquals(20, thirdTree.left.value);
+        assertEquals(15, thirdTree.left.left.value);
+        assertEquals(10, thirdTree.left.left.left.value);
+        assertEquals(22, thirdTree.left.right.value);
+        assertEquals(25, thirdTree.left.right.right.value);
+        assertEquals(90, thirdTree.right.value);
+        assertEquals(70, thirdTree.right.left.value);
+        assertEquals(95, thirdTree.right.right.value);
+        assertEquals(92, thirdTree.right.right.left.value);
+        assertEquals(100, thirdTree.right.right.right.value);
+
+        assertEquals(50, fourthTree.value);
 
     }
 
     @Test
     public void testContains() {
-        // First tree asserts
         assertTrue(firstTree.contains(10));
         assertTrue(firstTree.contains(15));
         assertTrue(firstTree.contains(0));
         assertTrue(firstTree.contains(20));
         assertTrue(firstTree.contains(5));
         assertTrue(firstTree.contains(8));
-
         assertFalse(firstTree.contains(70));
 
-        // Second tree asserts
         assertTrue(secondTree.contains(8));
         assertTrue(secondTree.contains(5));
         assertTrue(secondTree.contains(17));
         assertTrue(secondTree.contains(2));
         assertTrue(secondTree.contains(7));
+        assertTrue(secondTree.contains(4));
+        assertTrue(secondTree.contains(1));
         assertTrue(secondTree.contains(12));
         assertTrue(secondTree.contains(20));
-        assertTrue(secondTree.contains(4));
-
+        assertTrue(secondTree.contains(25));
         assertFalse(secondTree.contains(10));
 
-        // Third tree asserts
         assertTrue(thirdTree.contains(50));
+        assertTrue(thirdTree.contains(20));
+        assertTrue(thirdTree.contains(15));
+        assertTrue(thirdTree.contains(10));
+        assertTrue(thirdTree.contains(90));
+        assertTrue(thirdTree.contains(70));
+        assertTrue(thirdTree.contains(95));
+        assertTrue(thirdTree.contains(92));
+        assertTrue(thirdTree.contains(100));
+        assertFalse(thirdTree.contains(150));
 
-        assertFalse(thirdTree.contains(10));
+        assertTrue(fourthTree.contains(50));
+        assertFalse(fourthTree.contains(10));
     }
 
     @Test
     public void testDepth() {
-        // First tree asserts
         assertEquals(4, firstTree.depth());
 
-        // Second tree asserts
         assertEquals(4, secondTree.depth());
 
-        // Third tree asserts
-        assertEquals(1, thirdTree.depth());
+        assertEquals(4, thirdTree.depth());
+
+        assertEquals(1, fourthTree.depth());
     }
 
     @Test
     public void testToString() {
-        // First tree asserts
         assertEquals("0-5-8-10-15-20", firstTree.toString());
 
-        // Second tree asserts
-        assertEquals("2-4-5-7-8-12-17-20", secondTree.toString());
+        assertEquals("1-2-4-5-7-8-12-15-17-20-25", secondTree.toString());
 
-        // Third tree asserts
-        assertEquals("50", thirdTree.toString());
+        assertEquals("10-15-20-22-25-50-70-90-92-95-100", thirdTree.toString());
+
+        assertEquals("50", fourthTree.toString());
     }
 
     @Test
     public void testMin() {
-        // First tree asserts
         assertEquals(0, firstTree.min());
 
-        // Second tree asserts
-        assertEquals(2, secondTree.min());
+        assertEquals(1, secondTree.min());
 
-        // Third tree asserts
-        assertEquals(50, thirdTree.min());
+        assertEquals(10, thirdTree.min());
+
+        assertEquals(50, fourthTree.min());
+    }
+
+    @Test
+    public void testRemove() {
+        // Test removing node which doesn't have any children
+        firstTree.remove(8);
+        firstTree.remove(20);
+
+        assertFalse(firstTree.contains(8));
+        assertFalse(firstTree.contains(20));
+
+        // Test removing node which has one left leaf
+        secondTree.remove(2);
+        secondTree.remove(7);
+
+        assertFalse(secondTree.contains(2));
+        assertFalse(secondTree.contains(7));
+
+        // Test removing node which has one right leaf
+        secondTree.remove(12);
+        secondTree.remove(20);
+
+        assertFalse(secondTree.contains(12));
+        assertFalse(secondTree.contains(20));
+
+        // Test removing node which has two children
+        thirdTree.remove(20);
+        thirdTree.remove(90);
+
+        assertFalse(thirdTree.contains(20));
+        assertFalse(thirdTree.contains(90));
     }
 }
